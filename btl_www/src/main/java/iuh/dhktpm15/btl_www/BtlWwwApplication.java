896ae6d5060e935954c40c13e1eb60414b123cdf
LeKhoaVan <lekhoavan325802@gmail.com>
2023-04-12 21:34:02 +0700

    init

---
+++ btl_www/src/main/java/iuh/dhktpm15/btl_www/BtlWwwApplication.java
@@ -0,0 +1,13 @@
+package iuh.dhktpm15.btl_www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlWwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlWwwApplication.class, args);
+	}
+
+}
---
+++ btl_www/src/test/java/iuh/dhktpm15/btl_www/BtlWwwApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.dhktpm15.btl_www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlWwwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
